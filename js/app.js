12dd7c6b42d8fc5d40b52cf2d4c3cebf910aad33
ManiGeek <maniweeb@gmail.com>
2023-03-12 11:29:06 +0330

    feat:pain

---
+++ js/app.js
@@ -0,0 +1,35 @@
+window.addEventListener("load", () => {
+  const canvas = document.querySelector("#canvas");
+  const ctx = canvas.getContext("2d");
+
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  
+  let isPainting = false;
+
+  const startPainting=(e)=>{
+    isPainting=true;
+    draw(e)
+  }
+  const stopPainting=()=>{
+    isPainting=false;
+    ctx.beginPath()
+  }
+  const draw=(e)=>{
+    return
+  }
+
+  ctx.linecap='round'
+  ctx.lineWidth=10
+  ctx.lineTo(e.clientX, e.clientY)
+  ctx.stroke()
+  
+
+  window.addEventListener("mousedown", startPainting);
+  window.addEventListener("mouseup", stopPainting);
+  window.addEventListener("mousemove", draw);
+  window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  });
+});
